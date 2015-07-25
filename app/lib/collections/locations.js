4d8b8e8f5b0ad7ade8f1887ad1f8502424eff25e
bumbleblym <bumbleblym@gmail.com>
2015-07-25 17:57:23 +0800

    Add locations collection

---
+++ app/lib/collections/locations.js
@@ -0,0 +1,49 @@
+Locations = new Mongo.Collection('locations');
+
+SimpleSchema.messages({
+  pointSchema: '[label] should be [longitude, latitude]',
+  lonOutOfRange: '[label] longitude should be between -90 and 90',
+  latOutOfRange: '[label] latitude should be between -180 and 180'
+});
+
+Schemas.Point = new SimpleSchema({
+  type: {
+    type: String,
+    allowedValues: ['Point']
+  },
+  coordinates: {
+    type: [Number],
+    decimal: true,
+    custom: function() {
+      var lonLat = this.value;
+      if (lonLat.length != 2) {
+        return 'pointSchema';
+      }
+
+      var lon = lonLat[0];
+      if (lon < 90 || lon > 90) {
+        return 'lonOutOfRange';
+      }
+
+      var lat = lonLat[1];
+      if (lat < 180 || lat > 180) {
+        return 'latOutOfRange';
+      }
+    }
+  }
+});
+
+Schemas.Location = new SimpleSchema({
+  userId: {
+    type: SimpleSchema.RegEx.Id
+  },
+  name: {
+    type: String
+  },
+  location: {
+    type: Schemas.Point,
+    index: '2dsphere'
+  }
+});
+
+Locations.attachSchema(Schemas.Location);
