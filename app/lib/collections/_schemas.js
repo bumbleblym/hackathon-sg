d6e047c871e7c0678b65dc990ebd11bb1d3f88bf
bumbleblym <bumbleblym@gmail.com>
2015-07-25 22:00:34 +0800

    Add product schema

--- app/lib/collections/_schemas.js
+++ app/lib/collections/_schemas.js
@@ -1,6 +1,7 @@
 Schemas = {};
 
 SimpleSchema.messages({
+  notFound: '[value] [label] cannot be found',
   pointSchema: '[value] should be [longitude, latitude]',
   lonOutOfRange: '[value] longitude should be between -90 and 90',
   latOutOfRange: '[value] latitude should be between -180 and 180'
@@ -28,6 +29,38 @@ Schemas.Point = new SimpleSchema({
       var lat = lonLat[1];
       if (lat < 180 || lat > 180) {
         return 'latOutOfRange';
+      }
+    }
+  }
+});
+
+Schemas.Product = new SimpleSchema({
+  isOffering: {
+    type: Boolean
+  },
+  price: {
+    type: Number,
+    min: 0,
+    exclusiveMin: true,
+    decimal: true
+  },
+  disciplineId: {
+    type: SimpleSchema.RegEx.Id,
+    custom: function() {
+      if (Disciplines.findOne(this.value) === undefined) {
+        return notFound;
+      }
+    }
+  },
+  level: {
+    type: String,
+    label: 'Education level',
+    allowedValues: ['primary', 'secondary', 'post-secondary', 'tertiary', 'other'],
+    custom: function() {
+      var discipline = Disciplines.findOne(this.field('disciplineId').value);
+
+      if (!_.contains(discipline.levels, this.value)) {
+        return 'notAllowed';
       }
     }
   }
