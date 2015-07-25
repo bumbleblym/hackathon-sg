213c5454a558ec5305153fe95089f28782e8e579
bumbleblym <bumbleblym@gmail.com>
2015-07-25 22:47:18 +0800

    Upsert Facebook configuration

---
+++ app/server/service-configuration.js
@@ -0,0 +1,8 @@
+ServiceConfiguration.configurations.upsert({
+  service: 'facebook'
+}, {
+  $set: {
+    appId: '1639108626335963',
+    secret: '903e3d85c3a7c1211740ba13ed146373'
+  }
+});
--- app/server/social-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-  service: 'facebook'
-});
-
-ServiceConfiguration.configurations.insert({
-  service: 'facebook',
-  appId: '1639108626335963',
-  secret: '903e3d85c3a7c1211740ba13ed146373'
-});
