5afa6a8fbdaa58f2996df46445f0f1dd78cd2359
Wu Di <justwudi@gmail.com>
2015-07-26 01:55:09 +0800

    Implement placement of markers of the stupid map

--- app/client/templates/views/location/location.js
+++ app/client/templates/views/location/location.js
@@ -20,61 +20,111 @@ Template.location.helpers({
   }
 });
 
-GoogleMaps.ready('locationMap', function(locationMap) {
+Template.location.events({
+  'click #marker-done': function(e) {
+    var marker = Template.instance().currentMarker;
+    console.log(marker.getPosition());
+    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+    Template.instance().infoWindow.close();
+    Template.instance().currentMarker = null;
+  },
+  'click #marker-cancel': function(e) {
+    Template.instance().infoWindow.close();
+    Template.instance().currentMarker.setMap(null);
+    Template.instance().currentMarker = null;
+  }
+});
 
-  var markers = [];
-  var map = locationMap.instance;
+Template.location.onCreated(function() {
+  var self = this;
+  GoogleMaps.ready('locationMap', function(locationMap) {
 
-  // Create the search box and link it to the UI element.
-  var input = (document.getElementById('pac-input'));
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var map = locationMap.instance,
+        markers = [],
+        // Create the search box and link it to the UI element.
+        input = (document.getElementById('pac-input')),
+        searchBox = new google.maps.places.SearchBox(input);
 
-  var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  // Listen for the event fired when the user selects an item from the
-  // pick list. Retrieve the matching places for that item.
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-    var places = searchBox.getPlaces();
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+      var places = searchBox.getPlaces();
 
-    if (places.length === 0) {
-      return;
-    }
-    for (var i = 0, marker; marker = markers[i]; i++) {
-      marker.setMap(null);
-    }
+      if (places.length === 0) {
+        return;
+      }
+      for (var i = 0, marker; marker = markers[i]; i++) {
+        marker.setMap(null);
+      }
 
-    // For each place, get the icon, place name, and location.
-    markers = [];
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0, place; place = places[i]; i++) {
-      var image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
+      // For each place, get the icon, place name, and location.
+      markers = [];
+      var bounds = new google.maps.LatLngBounds();
+      for (var i = 0, place; place = places[i]; i++) {
+        var image = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
 
-      // Create a marker for each place.
-      var marker = new google.maps.Marker({
-        map: map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location
-      });
+        // Create a marker for each place.
+        var marker = new google.maps.Marker({
+          map: map,
+          icon: image,
+          title: place.name,
+          position: place.geometry.location
+        });
 
-      markers.push(marker);
+        markers.push(marker);
 
-      bounds.extend(place.geometry.location);
-    }
+        bounds.extend(place.geometry.location);
+      }
 
-    map.fitBounds(bounds);
-  });
+      map.fitBounds(bounds);
+    });
 
-  // Bias the SearchBox results towards places that are within the bounds of the
-  // current map's viewport.
-  google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-    searchBox.setBounds(bounds);
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+      var bounds = map.getBounds();
+      searchBox.setBounds(bounds);
+    });
+
+    var contentString = 
+      '<div id="content">'+
+        '<div class="message">Save location?</div>' +
+        '<div class="message-response">' +
+          '<a class="waves-effect waves-light btn-small" id="marker-cancel"><i class="material-icons left">cancel</i></a>' + 
+          '<a class="waves-effect waves-light btn-small" id="marker-done"><i class="material-icons left">done</i></a>' + 
+        '</div>' +
+      '</div>';
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+    self.infoWindow = infoWindow;
+
+    google.maps.event.addListener(map, 'click', function(e) {
+      if (self.currentMarker) {
+        self.currentMarker.setPosition(e.latLng);
+      } else {
+        self.currentMarker = new google.maps.Marker({
+          position: e.latLng, 
+          map: map,
+          icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+        });
+      }
+
+      infoWindow.open(map, self.currentMarker);
+    });
+
+    google.maps.event.addListener(infoWindow, 'closeclick', function() {
+      self.currentMarker.setMap(null);
+      self.currentMarker = null;
+    });
   });
 });
