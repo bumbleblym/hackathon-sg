9c446c39fa322990dfeb72d8e8296f898c68108d
bumbleblym <bumbleblym@gmail.com>
2015-07-26 10:21:36 +0800

    Fix profile links

--- app/client/templates/common/layout.js
+++ app/client/templates/common/layout.js
@@ -1,13 +1,23 @@
-Template.layout.onRendered(function() {
-  this.$('.button-collapse').sideNav();
-
-  this.$('.collapsible').collapsible({
-    accordion : false
-  });
+Template.layout.events({
+  'click a.login': function(event) {
+    Meteor.loginWithFacebook({}, function(err) {
+      if (err) {
+        Session.set('errorMessage', err.reason || 'Unknown error');
+      }
+    });
+  }
 });
 
 Template.layout.helpers({
   profilePictureUrl: function(id) {
     return 'http://graph.facebook.com/' + id + '/picture/?type=square';
   }
+});
+
+Template.layout.onRendered(function() {
+  this.$('.button-collapse').sideNav();
+
+  this.$('.collapsible').collapsible({
+    accordion : false
+  });
 });
--- app/client/templates/common/navItems.js
+++ app/client/templates/common/navItems.js
@@ -1,11 +1,4 @@
 Template.navItems.events({
-  'click a.login': function(event) {
-    Meteor.loginWithFacebook({}, function(err) {
-      if (err) {
-        Session.set('errorMessage', err.reason || 'Unknown error');
-      }
-    });
-  },
   'click a.logout': function(event) {
     Meteor.logout(function(err) {
       if (err) {
@@ -14,4 +7,3 @@ Template.navItems.events({
     });
   }
 });
-
